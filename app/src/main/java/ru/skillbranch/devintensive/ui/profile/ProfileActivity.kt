0f6370b041b231daff6d64a4ee9587a038d9d294
Alexey <kam@mstagency.ru>
2019-11-05 17:50:16 +0400

    50% of constrant layout

--- app/src/main/java/ru/skillbranch/devintensive/ui/profile/ProfileActivity.kt
+++ app/src/main/java/ru/skillbranch/devintensive/ui/profile/ProfileActivity.kt
@@ -10,7 +10,7 @@ class ProfileActivity : AppCompatActivity(){
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_profile_constraint)
+        setContentView(R.layout.activity_profile)
 
     }
 
