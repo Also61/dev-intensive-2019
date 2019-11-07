f457f2d8a6b2efcc1f56434865401572df70b0fc
Alexey <kam@mstagency.ru>
2019-11-07 11:59:07 +0400

    done task 1,2,3

---
+++ app/src/main/java/ru/skillbranch/devintensive/models/Profile.kt
@@ -0,0 +1,27 @@
+package ru.skillbranch.devintensive.models
+
+data class Profile (
+    val firstName:String,
+    val lastName:String,
+    val about:String,
+    val repository:String,
+    val raiting:Int = 0,
+    val respect:Int = 0){
+
+    val rank:String = "Junior Android Developer"
+    val nickName:String = "John Doe"
+
+    fun toMap():Map<String, Any> = mapOf(
+        "nickName" to nickName,
+        "rank" to rank,
+        "firstName" to firstName,
+        "lastName" to lastName,
+        "about" to about,
+        "repository" to repository,
+        "raiting" to raiting,
+        "respect" to respect
+    )
+
+
+
+}
--- app/src/main/java/ru/skillbranch/devintensive/ui/profile/ProfileActivity.kt
+++ app/src/main/java/ru/skillbranch/devintensive/ui/profile/ProfileActivity.kt
@@ -4,12 +4,18 @@ import android.graphics.ColorFilter
 import android.graphics.PorterDuff
 import android.graphics.PorterDuffColorFilter
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
 import kotlinx.android.synthetic.main.activity_profile.*
 import ru.skillbranch.devintensive.R
+import ru.skillbranch.devintensive.models.Profile
+import ru.skillbranch.devintensive.viewmodels.ProfileViewModel
 
 class ProfileActivity : AppCompatActivity(){
 
@@ -18,16 +24,41 @@ class ProfileActivity : AppCompatActivity(){
     }
 
 
-
+    private lateinit var viewModel: ProfileViewModel
     var isEditMode = false
     lateinit var viewFields : Map<String, TextView>
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        //TODO set custom theme this before super and setContenet View
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
         initViews(savedInstanceState)
+        initViewModels()
+        Log.d("M_ProfileActiviry","onCreate")
 
+    }
+
+    private fun initViewModels(){
+        viewModel = ViewModelProviders.of(this).get(ProfileViewModel::class.java)
+        viewModel.getProfileData().observe(this, Observer { UpdateUI(it) })
+        viewModel.getTheme().observe(this, Observer { updateTheme(it) })
+    }
+
+    private fun updateTheme(mode: Int) {
+        Log.d("M_ProfileActiviry","updateTheme")
+        delegate.setLocalNightMode(mode)
+
+    }
+
+    private fun UpdateUI(profile: Profile) {
+        profile.toMap().also {
+            for ((k,v) in viewFields){
+                v.text = it[k].toString()
+
+            }
+        }
     }
 
     private fun initViews(savedInstanceState: Bundle?) {
@@ -35,7 +66,7 @@ class ProfileActivity : AppCompatActivity(){
         isEditMode = savedInstanceState?.getBoolean(IS_EDIT_MODE, false) ?: false
 
         viewFields = mapOf(
-                "nickname" to tv_nick_name,
+                "nickName" to tv_nick_name,
                 "rank" to tv_rank,
                 "firstName" to et_first_name,
                 "lastName" to et_last_name,
@@ -46,9 +77,15 @@ class ProfileActivity : AppCompatActivity(){
         )
 
         btn_edit.setOnClickListener(View.OnClickListener {
+            if(isEditMode) saveProfileInfo()
             isEditMode = !isEditMode
             showCurrentMode(isEditMode)
         })
+
+        btn_switch_theme.setOnClickListener{
+            viewModel.switchTheme()
+
+        }
     }
 
     private fun showCurrentMode(isEdit: Boolean) {
@@ -77,15 +114,26 @@ class ProfileActivity : AppCompatActivity(){
             }
 
             val icon = if(isEdit){
-                resources.getDrawable(R.drawable.ic_save_black_24dp)
+                resources.getDrawable(R.drawable.ic_save_black_24dp,theme)
             }else{
-                resources.getDrawable(R.drawable.ic_edit_black_24dp)
+                resources.getDrawable(R.drawable.ic_edit_black_24dp,theme)
             }
 
             background.colorFilter = filter
             setImageDrawable(icon)
         }
 
+    }
+
+    private fun saveProfileInfo(){
+        Profile (
+                firstName = et_first_name.text.toString(),
+                lastName = et_last_name.text.toString(),
+                about = et_about.text.toString(),
+                repository = et_repository.text.toString()
+        ).apply {
+            viewModel.saveProfileData(this)
+        }
     }
 
 
